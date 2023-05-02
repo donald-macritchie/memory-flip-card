f9b50f98b23c6356f22d738024adfde4496b6342
donald-macritchie <>
2023-05-02 11:41:49 +0000

    flipCard function will check determine cardOnce form CardTwo

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,19 @@ document.addEventListener('DOMContentLoaded', function() {
     
     const icons = Array.from(cards);
 
+    let cardFlipped = false;
+    let cardOne;
+    let cardTwo;
+
     function flipCard() {
         this.classList.toggle('flipped');
+        if(cardFlipped === false) {
+            cardFlipped = true;
+            cardOne = this;
+        } else {
+            cardFlipped = false;
+            cardTwo = this;
+        }
     }
 
     icons.forEach(card => card.addEventListener('click', flipCard));
