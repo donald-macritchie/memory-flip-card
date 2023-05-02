f435a23bc5cabcf9c02373c5a3b24851d31f0aea
donald-macritchie <>
2023-05-02 11:13:02 +0000

    start JS with DOMContentLoaded

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    const cards = document.getElementsByClassName('game-card');
+    console.log(cards);
+    const icons = Array.from(cards);
+    console.log(icons);
+
+
+
+
+
+
+
+})
+
+console.log('hello')
