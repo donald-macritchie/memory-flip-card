8872e0e6b75e16ea35befa4ad4c4ba01e180dff1
donald-macritchie <>
2023-05-02 11:55:28 +0000

    add matching card function, if icons are the same, event listener is reemoved and cant be unflipped

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,10 +13,22 @@ document.addEventListener('DOMContentLoaded', function() {
         if(cardFlipped === false) {
             cardFlipped = true;
             cardOne = this;
+            //check if cardOne has been flipped
+            //any icon clicked after event will be false(not cardOne)
         } else {
             cardFlipped = false;
             cardTwo = this;
+            //if cardFlipped is set to true, the user will be clicking for cardTwo
+
+            matchingCards();
         }
+    }
+
+    function matchingCards() {
+        if(cardOne.dataset.icon === cardTwo.dataset.icon) {
+            cardOne.removeEventListener('click', flipCard);
+            cardTwo.removeEventListener('click', flipCard);
+        } 
     }
 
     icons.forEach(card => card.addEventListener('click', flipCard));
